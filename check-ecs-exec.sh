95d4e29733f724b04ded414ffd8aae00fbf9c318
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-03-27 00:43:39 +0900

    shebang must be placed at the first line.

--- check-ecs-exec.sh
+++ check-ecs-exec.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-#!/usr/bin/env bash
 # shellcheck disable=SC2059
 
 # Script Name: check-ecs-exec.sh
